7855cbfd8b85f90ac13d68d3842af39f1f818c56
Faizal <34762675+faizalzafri@users.noreply.github.com>
2026-08-08 22:46:47 +0530

    Init
    - Project structure
    - Build config
    - Dependencies

---
+++ src/test/java/com/remitmind/ai/RemitMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitmind.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
